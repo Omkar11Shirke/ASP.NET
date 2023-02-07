36c974cb823849653e4ec5f2931b36367c2a7afe
Omkar <122523386+Omkar11Shirke@users.noreply.github.com>
2023-02-08 01:05:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac1_b_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Enter the number 1: ");
+            n1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number 2: ");
+            n2 = Int32.Parse(Console.ReadLine());
+
+            if (n1 > 10 && n2 > 10)
+            {
+                Console.WriteLine("ACCEPTABLE");
+            }
+            else
+            {
+                Console.WriteLine("NOT ACCEPTABLE");
+            }
+            Console.Read();
+        }
+    }
+}
