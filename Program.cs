fea14710db1c2e50ae96bb741f785e14fbfa67ef
Omkar <122523386+Omkar11Shirke@users.noreply.github.com>
2023-02-08 01:06:16 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Prac1_b_
+namespace Prac1_a_
 {
     internal class Program
     {
@@ -12,13 +12,17 @@ namespace Prac1_b_
         {
             int n1, n2;
             Console.WriteLine("Enter the number 1: ");
-            n1 = Int32.Parse(Console.ReadLine());
+            n1=Int32.Parse(Console.ReadLine());
             Console.WriteLine("Enter the number 2: ");
             n2 = Int32.Parse(Console.ReadLine());
 
-            if (n1 > 10 && n2 > 10)
+            if (n1>10 && n2<10)
             {
-                Console.WriteLine("ACCEPTABLE");
+                Console.WriteLine("N1 is Greater");
+            }
+            else if(n2>10 && n1<10)
+            {
+                Console.WriteLine("N2 is Greater");
             }
             else
             {
